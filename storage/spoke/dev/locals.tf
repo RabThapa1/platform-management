13410df1d318bdca488f6244aed2b87215a969bf
RabThapa1 <138276412+RabThapa1@users.noreply.github.com>
2024-02-18 16:34:07 +1300

    update local.tf

--- storage/spoke/dev/locals.tf
+++ storage/spoke/dev/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name                = "sa_d_st_account"
   account_tier        = "standard"
-  account_replication = "GRS"
+  account_replication_type = "GRS"
   resource_group_name = "rg_d_st_account"
   location            = "australiaeast"
 }
