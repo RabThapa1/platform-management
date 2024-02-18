4e1f380d7bd28fac67d19b9828b8b3ea67ed2f10
RabThapa1 <138276412+RabThapa1@users.noreply.github.com>
2024-02-18 14:51:25 +1300

    Add initial files

---
+++ storage/module/variables.tf
@@ -0,0 +1,14 @@
+variable "name"{
+    description = "Name for the storage account"
+    type = string
+}
+
+variable "account_tier"{
+    description = "Account tier type standard, premium etc"
+    type = string
+}
+
+variable "account_replication"{
+    description = "Account replication like LRS, GRS, GZRS etc"
+    type = string
+}
