5e286cb23d953ef41538506db85f7562255db44e
RabThapa1 <138276412+RabThapa1@users.noreply.github.com>
2024-03-15 11:32:49 +1300

    Update version.tf

--- version.tf
+++ version.tf
@@ -5,4 +5,11 @@ terraform  {
             version = "3.93.0"
         }
     }
+
+    backend "azurerm"{
+        resource_group_name = "rg_d_st_account"
+        storage_account_name = "sahtfstatetinfosolution"
+        container_name = "tfstate"
+        key = "platform_management.terraform.tfstate"
+    }
 }
